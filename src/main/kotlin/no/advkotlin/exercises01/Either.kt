5fb82fb57c6e322668b5f394ba833333c59c46d4
Bjørn Hamre <bha@ambita.com>
2022-08-23 18:13:23 +0200

    Moved exercises src and test to it's own directory

--- src/main/kotlin/no/advkotlin/exercises01/Either.kt
+++
@@ -1,5 +0,0 @@
-package no.advkotlin.exercises01
-
-/*
-This is where you will implement the Either type
-*/
--- src/main/kotlin/no/advkotlin/exercises01/Extensions.kt
+++
@@ -1,5 +0,0 @@
-package no.advkotlin.exercises01
-
-/*
-This is the file where you will implement the extension functions from part 1 of the exercises
-*/
