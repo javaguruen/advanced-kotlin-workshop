5e398b2a5dc9c6903a91c5dcf368365aa7e40421
Morten Nygaard Åsnes <morten.aasnes@gmail.com>
2022-09-04 17:07:54 +0200

    løsninger for UserExercises

--- exercises03/src/main/kotlin/no/advkotlin/exercises03/userservice/UserExercises.kt
+++ exercises03/src/main/kotlin/no/advkotlin/exercises03/userservice/UserExercises.kt
@@ -1,14 +1,33 @@
 package no.advkotlin.exercises03.userservice
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import no.advkotlin.exercises03.userservice.domain.Product
 import no.advkotlin.exercises03.userservice.domain.User
 import no.advkotlin.exercises03.userservice.service.UserService
 
 class UserExercises(val userService: UserService) {
 
-    suspend fun getUserAndItemsCoroutine(userId: String): Pair<User, List<Product>> = TODO()
+    suspend fun getUserAndItemsCoroutine(userId: String): Pair<User, List<Product>> = coroutineScope {
+        userService.getUser(userId) to userService.getProducts(userId)
+    }
 
-    suspend fun getUserAndItemsConcurrent(userId: String): Pair<User, List<Product>> = TODO()
+    suspend fun getUserAndItemsConcurrent(userId: String): Pair<User, List<Product>> = coroutineScope {
+        val user = async {
+            userService.getUser(userId)
+        }
+        val products = async {
+            userService.getProducts(userId)
+        }
+        user.await() to products.await()
+    }
 
-    suspend fun getUsersAndItemsInBulk(userIds: List<String>): List<Pair<User, List<Product>>> = TODO()
+    suspend fun getUsersAndItemsInBulk(userIds: List<String>): List<Pair<User, List<Product>>> = coroutineScope {
+        val usersAndProducts = userIds.map {
+            async { userService.getUser(it) } to async { userService.getProducts(it) }
+        }
+        usersAndProducts.map {
+            it.first.await() to it.second.await()
+        }
+    }
 }
--- exercises03/src/test/kotlin/no/advkotlin/exercises03/userservice/UserExercisesTest.kt
+++ exercises03/src/test/kotlin/no/advkotlin/exercises03/userservice/UserExercisesTest.kt
@@ -11,32 +11,32 @@ import org.junit.jupiter.api.Assertions.*
 @OptIn(ExperimentalCoroutinesApi::class)
 internal class UserExercisesTest {
 
-//    @Test
-//    fun getUserAndItemsCoroutine() = runTest {
-//        val userExercises = UserExercises(UserService())
-//        val result = userExercises.getUserAndItemsCoroutine("101")
-//        assertEquals("Signy Evensen", result.first.name)
-//        assertEquals(1, result.second.size)
-//        assertEquals(2000, currentTime)
-//    }
+    @Test
+    fun getUserAndItemsCoroutine() = runTest {
+        val userExercises = UserExercises(UserService())
+        val result = userExercises.getUserAndItemsCoroutine("101")
+        assertEquals("Signy Evensen", result.first.name)
+        assertEquals(1, result.second.size)
+        assertEquals(2000, currentTime)
+    }
 
-//    @Test
-//    fun getUserAndItemsConcurrent() = runTest {
-//        val userExercises = UserExercises(UserService())
-//        val result = userExercises.getUserAndItemsConcurrent("101")
-//        assertEquals("Signy Evensen", result.first.name)
-//        assertEquals(1, result.second.size)
-//        assertEquals(1000, currentTime)
-//    }
+    @Test
+    fun getUserAndItemsConcurrent() = runTest {
+        val userExercises = UserExercises(UserService())
+        val result = userExercises.getUserAndItemsConcurrent("101")
+        assertEquals("Signy Evensen", result.first.name)
+        assertEquals(1, result.second.size)
+        assertEquals(1000, currentTime)
+    }
 
-//    @Test
-//    fun getUsersAndItemsInBulk() = runTest {
-//        val userExercises = UserExercises(UserService())
-//        val result = userExercises.getUsersAndItemsInBulk(listOf("101", "102", "103"))
-//        assertEquals(3, result.size)
-//        val user = result.first()
-//        assertEquals("Signy Evensen", user.first.name)
-//        assertEquals(1, user.second.size)
-//        assertEquals(1000, currentTime)
-//    }
+    @Test
+    fun getUsersAndItemsInBulk() = runTest {
+        val userExercises = UserExercises(UserService())
+        val result = userExercises.getUsersAndItemsInBulk(listOf("101", "102", "103"))
+        assertEquals(3, result.size)
+        val user = result.first()
+        assertEquals("Signy Evensen", user.first.name)
+        assertEquals(1, user.second.size)
+        assertEquals(1000, currentTime)
+    }
 }
